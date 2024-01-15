64030156d3466ba430a65d512fb0d9bda6a7ad5d
roxanamanea29 <156657309+roxanamanea29@users.noreply.github.com>
2024-01-15 22:51:01 +0100

    Delete trabajointerfaces directory

--- trabajointerfaces/src/main/java/com/mycompany/trabajointerfaces/iDelantero.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.trabajointerfaces;
-
-
-public interface iDelantero {// iDelantero contiene las siguientes métodos 
-    public void rematar();
-    public void hacerRegate();
-    public void definirGol();
-}
--- trabajointerfaces/src/main/java/com/mycompany/trabajointerfaces/iPortero.java
+++
@@ -1,9 +0,0 @@
-
-package com.mycompany.trabajointerfaces;
-
-public interface iPortero {  // iPortero contiene las siguientes métodos que las va ha utilizar la clase PorteroGoleador
-    public void atajarBalon();
-    public void despejarCentro();
-    public void sacarPorteria();
-    
-}
